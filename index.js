2793908368f63787980acc841a97018771ee539d
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-18 19:33:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue) {
+  return array.filter(function(element){
+    return element[`revenue`] > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(array, revenue) {
+  return driversWithRevenueOver(array, revenue)
+    .map(function(element){
+      return element.name;
+    });
+}
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList(array, object) {
+  return exactMatch(array, object)
+  .map(function(element){
+    return element.name;
+  });
+}
